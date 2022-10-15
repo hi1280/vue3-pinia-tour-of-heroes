6654021542c35fd2db8d820fa1766aa05cd453ec
hi1280 <6506310+hi1280@users.noreply.github.com>
2022-10-15 22:00:57 +0900

    install vue3 and vite

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import createVuePlugin from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      vue: '@vue/compat'
+    }
+  },
+  plugins: [
+    createVuePlugin({
+      template: {
+        compilerOptions: {
+          compatConfig: {
+            MODE: 2
+          }
+        }
+      }
+    })
+  ]
+})
